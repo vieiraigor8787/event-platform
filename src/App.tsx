15af41fe59c8476eefe6eccb1042aa5b1e5eb515
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-08-02 10:16:23 -0300

    config react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client";
+import { Router } from "./Router";
+import { client } from "./lib/apollo";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-return (
-    <Event />
-  )
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
