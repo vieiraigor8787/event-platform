6c9412d0dc54371b05a90e136ad69a3f07695494
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-08-02 15:45:27 -0300

    handlesubscribe

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,7 @@
 import { format, isPast } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR"
 import { CheckCircle, Lock } from "phosphor-react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -11,16 +11,20 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{slug: string}>()
+
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormat = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR
   })
 
+  const isActiveLesson = slug === props.slug;
+
   return (
     <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormat}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' :'' }`}>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -31,7 +35,7 @@ export function Lesson(props: LessonProps) {
               <Lock size={20} /> Em breve
             </span>
           )}
-          <span className="text-xs rounded px-2 py-[2px] text-green-300 border border-green-300 font-bold">
+          <span className={`text-xs rounded px-2 py-[2px] text-green-300 border border-green-300 font-bold ${isActiveLesson ? 'text-white' : ''}`}>
             {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
