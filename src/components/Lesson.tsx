71d7a308d652674c39af36c8f151d3a43e8c33cb
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-08-02 10:40:25 -0300

    change <a> by <Link> & config router component

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { format, isPast } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR"
 import { CheckCircle, Lock } from "phosphor-react";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -16,10 +17,10 @@ export function Lesson(props: LessonProps) {
   })
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormat}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -37,6 +38,6 @@ export function Lesson(props: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
