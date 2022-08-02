4f7fdc62294a93997da2569aac2cc1daeee656b4
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-08-02 10:01:43 -0300

    installing and input vime

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,4 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import {
   CaretRight,
   DiscordLogo,
@@ -5,12 +6,17 @@ import {
   Lightning,
 } from "phosphor-react";
 
+import '@vime/core/themes/default.css';
+
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-          
+          <Player>
+            <Youtube videoId='KxNkjsowLpU' />
+            <DefaultUi />
+          </Player>
         </div>
       </div>
 
