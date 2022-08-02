49b1c7a1f179236f87932c6102ee32c22b248fd4
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-08-02 14:53:49 -0300

    auth token content api

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/cl4pz7yzi2lv101xsego8h0jn/master',
+  uri: import.meta.env.VITE_API_URL, 
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
